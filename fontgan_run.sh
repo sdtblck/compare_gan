e7c9f69d7f2a5be4c0dc5fe72e00625a1a3dbd83
sdtblck <46172032+sdtblck@users.noreply.github.com>
2020-06-10 22:35:40 +0100

    Update and rename run.sh to fontgan_run.sh

---
+++ fontgan_run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -ex
+export TPU_HOST=10.6.0.5
+export TPU_NAME="${TPU_NAME:-tpu-v3-128-euw4a-4}"
+export MODEL_DIR="${MODEL_DIR:-gs://fontgan_euw4/model_runs/fonts_128_1}"
+#export DATASETS=gs://danbooru-euw4a/datasets/danbooru2019-s/danbooru2019-s-0*,gs://danbooru-euw4a/datasets/e621-s/e621-s-0*
+#export DATASETS=gs://danbooru-euw4a/datasets/danbooru2019-s/danbooru2019-s-0*
+export DATASETS=gs://fontgan_euw4/datasets/fonts_128/fonts_128-0*
+export GIN_CONFIG="${GIN_CONFIG:-example_configs/biggan_font128.gin}"
+exec python3 wrapper.py compare_gan/main.py --use_tpu --tfds_data_dir 'gs://danbooru-euw4a/tensorflow_datasets/' --model_dir "${MODEL_DIR}" --gin_config "$GIN_CONFIG"
